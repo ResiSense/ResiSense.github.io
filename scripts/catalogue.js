ae3ed123f32a62829527f92a7ff95e79820c2be6
CCheukKa <ccheukka@gmail.com>
2024-01-29 03:22:29 +0000

    Add catalogue dividers

--- scripts/catalogue.js
+++ scripts/catalogue.js
@@ -1,18 +1,27 @@
 const catalogueElement = document.getElementById("catalogue");
 const catalogueItemTemplateElement = document.getElementById("catalogue-item-template");
+const catalogueDividerTemplateElement = document.getElementById("catalogue-divider-template");
 
-getPageConfig().then(pageConfig => {
-    listPages(pageConfig).forEach(page => {
-        // console.log(page);
-        const catalogueItemElement = catalogueItemTemplateElement.content.cloneNode(true);
-        catalogueItemElement.querySelector("a").href = page.path;
-        catalogueItemElement.querySelector("a").textContent = page.name;
-        if (page.path.replace(/^\//, '') === currentPathTree.join('/')) {
-            catalogueItemElement.querySelector("div").classList.add("catalogue-item-active");
-        }
-        catalogueElement.appendChild(catalogueItemElement);
+
+buildCatalogue();
+
+function buildCatalogue() {
+    getPageConfig().then(pageConfig => {
+        listPages(pageConfig).forEach(page => {
+            // console.log(page);
+            const catalogueItemElement = catalogueItemTemplateElement.content.cloneNode(true);
+            catalogueItemElement.querySelector("a").href = page.path;
+            catalogueItemElement.querySelector("a").textContent = page.name;
+            if (page.path.replace(/^\//, '') === currentPathTree.join('/')) {
+                catalogueItemElement.querySelector("div").classList.add("catalogue-item-active");
+            }
+            catalogueElement.appendChild(catalogueItemElement);
+
+            const catalogueDividerElement = catalogueDividerTemplateElement.content.cloneNode(true);
+            catalogueElement.appendChild(catalogueDividerElement);
+        });
     });
-});
+}
 
 function listPages(pageConfig, pathTrace = '') {
     const pages = [];
