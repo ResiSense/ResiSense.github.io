2f47c76fa332c5cc64d96aab91396a0414b7dbe9
CCheukKa <ccheukka@gmail.com>
2024-01-30 21:35:06 +0000

    Add sticky page title

--- scripts/legend.js
+++ scripts/legend.js
@@ -1,3 +1,11 @@
 const contentLegendElement = document.getElementById('content-legend');
-
 contentLegendElement.innerHTML = document.title;
+
+const headerItemsContainerElement = document.getElementsByClassName('header-items-container')[0];
+window.addEventListener('scroll', () => {
+    if (contentLegendElement.getBoundingClientRect().top < headerItemsContainerElement.getBoundingClientRect().bottom) {
+        contentLegendElement.classList.add('hide-text-shadow');
+    } else {
+        contentLegendElement.classList.remove('hide-text-shadow');
+    }
+});
