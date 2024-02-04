115e17a7a0c5d0e7e82c8e6911e692dad4f37cca
CCheukKa <ccheukka@gmail.com>
2024-02-04 02:39:34 +0000

    Fix race condition

--- scripts/legend.js
+++ scripts/legend.js
@@ -1,9 +1,9 @@
 const contentLegendElement = document.getElementById('content-legend');
 contentLegendElement.innerHTML = document.title;
 
-const headerItemsContainerElement = document.getElementsByClassName('header-items-container')[0];
+const headerItemsContainerElement = document.getElementsByClassName('header-items-container');
 window.addEventListener('scroll', () => {
-    if (contentLegendElement.getBoundingClientRect().top < headerItemsContainerElement.getBoundingClientRect().bottom) {
+    if (contentLegendElement.getBoundingClientRect().top < headerItemsContainerElement[0].getBoundingClientRect().bottom) {
         contentLegendElement.classList.add('hide-text-shadow');
     } else {
         contentLegendElement.classList.remove('hide-text-shadow');
