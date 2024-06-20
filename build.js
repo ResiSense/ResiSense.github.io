59082d2a86baad7e4acf287a53504e43451df43a
CCheukKa <ccheukka@gmail.com>
2024-06-20 05:31:18 +0100

    chore: Update file path in build.js

--- build.js
+++ build.js
@@ -3,4 +3,4 @@ console.log('build.js is running...')
 const fs = require('fs-extra');
 const path = require('path');
 
-fs.writeFileSync(path.resolve('prod', 'test.txt'), 'Hello World!');
+fs.writeFileSync('prod/test.txt', 'Hello World!');
