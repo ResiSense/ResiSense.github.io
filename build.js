ad92992fefeda7a59560ac45e6a58b7687eaba80
CCheukKa <ccheukka@gmail.com>
2024-06-20 05:26:09 +0100

    yaml test

--- build.js
+++ build.js
@@ -3,4 +3,4 @@ console.log('build.js is running...')
 const fs = require('fs-extra');
 const path = require('path');
 
-fs.writeFileSync(path.resolve(__dirname, 'build.txt'), 'Hello, build.txt!');
+fs.writeFileSync(path.resolve(__dirname, 'prod', 'test.txt'), 'Hello World!');
