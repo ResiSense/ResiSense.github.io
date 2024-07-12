dc610318551e6e37895bb63ddc164f009322b311
CCheukKa <ccheukka@gmail.com>
2024-07-13 05:01:25 +0800

    add debug logs

--- .gitlab/_plugins/url_filter.rb
+++ .gitlab/_plugins/url_filter.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 
 Jekyll::Hooks.register :pages, :post_render do |page|
+    puts "Running URL filter for page: #{page.url}"
     site_baseurl = page.site.baseurl
     next if site_baseurl.empty?
 
