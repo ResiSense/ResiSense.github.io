fbb8d770b758c6eda47017bd1eee70a2970c5fc4
CCheukKa <ccheukka@gmail.com>
2024-07-13 04:46:55 +0800

    fix links to root

---
+++ .gitlab/_plugins/url_filter.rb
@@ -0,0 +1,17 @@
+require 'nokogiri'
+
+Jekyll::Hooks.register :pages, :post_render do |page|
+    site_baseurl = page.site.baseurl
+    next if site_baseurl.empty?
+
+    doc = Nokogiri::HTML(page.output)
+    doc.css('a').each do |link|
+        href = link['href']
+        next unless href.start_with?('/')
+        next if href.start_with?("#{site_baseurl}/")
+
+        link['href'] = "#{site_baseurl}#{href}"
+    end
+
+    page.output = doc.to_html
+end
