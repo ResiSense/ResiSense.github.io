e9dace96114de78f55de7fea2ac3c1144ea750b5
CCheukKa <ccheukka@gmail.com>
2024-07-14 23:23:44 +0800

    improve performance slightly on scroll behaviour for content legend

--- scripts/legend.js
+++ scripts/legend.js
@@ -6,9 +6,6 @@ if (window.location.pathname.split('/').includes('404')) {
 const contentLegendElement = document.getElementById('content-legend');
 const headerItemsContainerElement = document.getElementsByClassName('header-items-container');
 window.addEventListener('scroll', () => {
-    if (contentLegendElement.getBoundingClientRect().top < headerItemsContainerElement[0].getBoundingClientRect().bottom) {
-        contentLegendElement.classList.add('hide-text-shadow');
-    } else {
-        contentLegendElement.classList.remove('hide-text-shadow');
-    }
+    const shouldHideContentLegend = contentLegendElement.getBoundingClientRect().top < headerItemsContainerElement[0].getBoundingClientRect().bottom;
+    contentLegendElement.classList.toggle('hide-text-shadow', shouldHideContentLegend);
 });
