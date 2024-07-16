9e7a1800303f86cfc96177d881f91b1cc78a7965
CCheukKa <ccheukka@gmail.com>
2024-07-16 09:58:07 +0800

    add footer banner with brand, affiliation, and sponsors

---
+++ scripts/footer.js
@@ -0,0 +1,8 @@
+const footerSpacerElement = document.querySelector('.footer-spacer');
+const footerPersistentElement = document.querySelector('.footer-persistent');
+
+document.addEventListener('DOMContentLoaded', calculateFooterSpacerHeight);
+function calculateFooterSpacerHeight() {
+    const footerPersistentHeight = footerPersistentElement.clientHeight;
+    footerSpacerElement.style.setProperty('--footer-spacer-height', `${footerPersistentHeight}px`);
+}
