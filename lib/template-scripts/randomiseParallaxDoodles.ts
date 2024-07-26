f832ff34a7f9c2d8d64de9045a3fb01c0ee331ef
CCheukKa <ccheukka@gmail.com>
2024-07-27 00:48:40 +0800

    make doodles scroll at the same speed for all page sizes

--- lib/template-scripts/randomiseParallaxDoodles.ts
+++ lib/template-scripts/randomiseParallaxDoodles.ts
@@ -1,8 +1,6 @@
 import { PageData } from "../types/PageData";
 import fs = require('fs-extra');
 
-
-const Y_PAGE_SIZE_LIMIT = 10000; // in px
 const Y_DENSITY_PER_PX = 5 / 1000; // in count per px
 const GRADATION_HEIGHT = 100; // in px
 
@@ -10,8 +8,17 @@ function randomFromRange(min: number, max: number) {
     return Math.random() * (max - min) + min;
 }
 
-function cloneDoodlesToFillPool(parallaxElement: HTMLElement) {
-    const requiredDoodleCount = Y_DENSITY_PER_PX * Y_PAGE_SIZE_LIMIT;
+function calculatePageSizeHeuristic(contentLength: number): number {
+    const x = contentLength;
+    //! This is a magic number heuristic formula from regression
+    return -9E-06 * x ** 2 + 0.3999 * x + 633.12; // quadratic
+    // return 0.2332 * x + 1147.2; // linear
+    // return 10000; // constant
+}
+
+function cloneDoodlesToFillPool(parallaxElement: HTMLElement, Y_PAGE_SIZE_HEURISTIC: number) {
+    // 
+    const requiredDoodleCount = Y_DENSITY_PER_PX * Y_PAGE_SIZE_HEURISTIC * 2;
     // make a doodle bag to clone from
     const doodleBag: HTMLElement[] = Array.from(parallaxElement.children).map(child => child.cloneNode(true) as HTMLElement);
     // fill the pool
@@ -45,20 +52,30 @@ function calculateRandomDoodleOffset(siblingCount: number) {
     return { xOffset, yOffset };
 }
 
+function calculateRandomTranslateAmount(Y_PAGE_SIZE_HEURISTIC: number) {
+    const MEDIAN_FACTOR = 17e-5;
+    const DEVIATION_FACTOR = MEDIAN_FACTOR / 2;
+    const median = -Y_PAGE_SIZE_HEURISTIC * Y_PAGE_SIZE_HEURISTIC * MEDIAN_FACTOR;
+    const deviation = Y_PAGE_SIZE_HEURISTIC * Y_PAGE_SIZE_HEURISTIC * DEVIATION_FACTOR;
+    return randomFromRange(median - deviation, median + deviation);
+}
+
 export default function (pageData: PageData) {
     const PARALLAX_CSS_PATH = `${pageData.targetDirectory}/styles/parallax.css`;
     const document = pageData.document;
     // 
     const parallaxElement = document.getElementById('parallax');
     const parallaxAnimations: { hash: string, translateAmount: number }[] = [];
-    cloneDoodlesToFillPool(parallaxElement);
+    const contentLength = document.getElementsByTagName('painted-content')[0].innerHTML.length;
+    const Y_PAGE_SIZE_HEURISTIC = calculatePageSizeHeuristic(contentLength);
+    cloneDoodlesToFillPool(parallaxElement, Y_PAGE_SIZE_HEURISTIC);
     for (let child of parallaxElement.children) {
         const doodleElement = child as HTMLElement;
         // 
         const { xOffset, yOffset } = calculateRandomDoodleOffset(parallaxElement.childElementCount);
         doodleElement.style.translate = `${xOffset}vw ${yOffset}px`;
         //
-        const translateAmount = randomFromRange(-2000, -6000);
+        const translateAmount = calculateRandomTranslateAmount(Y_PAGE_SIZE_HEURISTIC);
         const hash = translateAmount.toString(36).substring(7);
         doodleElement.style.animationName = `parallax-${hash}`;
         parallaxAnimations.push({ hash, translateAmount });
