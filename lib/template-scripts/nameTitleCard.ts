948338ed7bf03a7e13e902b209a7726dad5e23fe
CCheukKa <ccheukka@gmail.com>
2024-07-28 01:10:23 +0800

    add responsive title

--- lib/template-scripts/nameTitleCard.ts
+++ lib/template-scripts/nameTitleCard.ts
@@ -3,7 +3,7 @@ import { PageData } from "../types/PageData";
 export default function (pageData: PageData) {
     const document = pageData.document;
     // 
-    for (const element of document.getElementsByClassName('card-title')) {
+    for (const element of document.getElementsByClassName('page-title')) {
         element.innerHTML = document.title;
     }
 }
--- scripts/responsivenessHandler.js
+++ scripts/responsivenessHandler.js
@@ -4,10 +4,22 @@ const contentScrollPastHeader = {
 }
 
 window.addEventListener('DOMContentLoaded', determineContentScrollPastHeader);
-window.addEventListener("scroll", determineContentScrollPastHeader);
+window.addEventListener('scroll', determineContentScrollPastHeader);
 function determineContentScrollPastHeader() {
     contentScrollPastHeader.current = window.scrollY > parseFloat(getComputedStyle(document.documentElement).fontSize);
     if (contentScrollPastHeader.current === contentScrollPastHeader.old) { return; }
     contentScrollPastHeader.old = contentScrollPastHeader.current;
     window.dispatchEvent(new CustomEvent(eventType.contentScrollPastHeader, { detail: contentScrollPastHeader.current }));
+}
+
+window.addEventListener('DOMContentLoaded', determineShowHeaderPageTitle);
+window.addEventListener('scroll', determineShowHeaderPageTitle);
+const headerPageTitleElement = document.getElementById('header-page-title');
+const headerElement = document.getElementsByTagName('header')[0];
+const mainElement = document.getElementsByTagName('main')[0];
+function determineShowHeaderPageTitle() {
+    const mainRectTop = mainElement.getBoundingClientRect().top;
+    const headerSpacerRectBottom = headerElement.getBoundingClientRect().bottom;
+    const showHeaderPageTitle = mainRectTop <= headerSpacerRectBottom;
+    headerPageTitleElement.classList.toggle('hidden', !showHeaderPageTitle);
 }
