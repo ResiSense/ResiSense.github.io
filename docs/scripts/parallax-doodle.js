0bf78a8d2f8ee80dafab6534ccea296bdde58af5
CCheukKa <ccheukka@gmail.com>
2024-07-31 03:11:22 +0800

    STATIC BUILD: chore: Update build script to handle multi-line commit messages [skip ci] (#55)
    Co-authored-by: CCheukKa <CCheukKa@users.noreply.github.com>

---
+++ docs/scripts/parallax-doodle.js
@@ -0,0 +1,8 @@
+console.log(`Script running: ${document.currentScript.src}`);
+//
+const parallaxDoodleContainer = document.getElementById('parallax-doodle-container');
+
+window.addEventListener('scroll', () => {
+    const yOffset = window.scrollY;
+    parallaxDoodleContainer.style.translate = `0 -${yOffset}px`;
+});
