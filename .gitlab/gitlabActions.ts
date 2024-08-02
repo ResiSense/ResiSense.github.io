77a947033bbc634edcb1d1895c4aa826fbcaed58
CCheukKa <ccheukka@gmail.com>
2024-08-03 00:37:25 +0800

    add debug logs

--- .gitlab/gitlabActions.ts
+++ .gitlab/gitlabActions.ts
@@ -2,14 +2,14 @@ import igemUploadApi, { RemoteResourceData } from 'igem-upload-api';
 import * as fs from 'fs-extra';
 import { glob } from 'glob';
 
-const ARG2 = process.argv[2];
-const ARG3 = process.argv[3];
-const ARG4 = process.argv[4];
-if (!ARG2 || !ARG3 || !ARG4) { throw new Error('Missing environment variables. Expected: [TEAM_NUMBER] [USERNAME] [PASSWORD]'); }
-const TEAM_NUMBER = +ARG2;
-const USERNAME = ARG3;
-const PASSWORD = ARG4;
-if (!Number.isSafeInteger(TEAM_NUMBER)) { throw new Error('TEAM_NUMBER must be a number. Expected: [TEAM_NUMBER] [USERNAME] [PASSWORD]'); }
+// const ARG2 = process.argv[2];
+// const ARG3 = process.argv[3];
+// const ARG4 = process.argv[4];
+// if (!ARG2 || !ARG3 || !ARG4) { throw new Error('Missing environment variables. Expected: [TEAM_NUMBER] [USERNAME] [PASSWORD]'); }
+// const TEAM_NUMBER = +ARG2;
+// const USERNAME = ARG3;
+// const PASSWORD = ARG4;
+// if (!Number.isSafeInteger(TEAM_NUMBER)) { throw new Error('TEAM_NUMBER must be a number. Expected: [TEAM_NUMBER] [USERNAME] [PASSWORD]'); }
 const remoteDirectoryPath = ['assets'];
 const localDirectoryPath = ['docs', 'assets'];
 
@@ -19,50 +19,50 @@ const htmlFiles = glob.sync(`${PROD_DIRECTORY}/**/*.html`);
 const cssFiles = glob.sync(`${PROD_DIRECTORY}/**/*.css`);
 
 (async () => {
-    const remoteResourceData: RemoteResourceData[] = await uploadAssets();
-    await relinkUrls(remoteResourceData);
+    // const remoteResourceData: RemoteResourceData[] = await uploadAssets();
+    // await relinkUrls(remoteResourceData);
     await changeBaseUrlTags();
     await fs.remove(localDirectoryPath.join('/'));
 })();
 
-async function uploadAssets(): Promise<RemoteResourceData[]> {
-    const api = new igemUploadApi(TEAM_NUMBER, USERNAME, PASSWORD);
-    await api.startSession();
+// async function uploadAssets(): Promise<RemoteResourceData[]> {
+//     const api = new igemUploadApi(TEAM_NUMBER, USERNAME, PASSWORD);
+//     await api.startSession();
 
-    await api.purgeDirectory(remoteDirectoryPath, true);
-    const remoteResourceData = await api.uploadDirectory(remoteDirectoryPath, localDirectoryPath);
+//     await api.purgeDirectory(remoteDirectoryPath, true);
+//     const remoteResourceData = await api.uploadDirectory(remoteDirectoryPath, localDirectoryPath);
 
-    await api.endSession();
-    return remoteResourceData;
-}
+//     await api.endSession();
+//     return remoteResourceData;
+// }
 
-async function relinkUrls(remoteResourceData: RemoteResourceData[]) {
-    const htmlRegex = /(?<=(href|src)=")\/.*?(?=")/g;
-    const cssRegex1 = /(?<=url\(')\/.*?(?='\))/g;
-    const cssRegex2 = /(?<=url\(")\/.*?(?="\))/g;
-    // 
-    await Promise.all([
-        ...htmlFiles.map(filePath => relinkFileUrls(filePath, [htmlRegex])),
-        ...cssFiles.map(filePath => relinkFileUrls(filePath, [cssRegex1, cssRegex2]))
-    ]);
-    return;
-    // 
-    async function relinkFileUrls(filePath: string, regexes: RegExp[]) {
-        console.log(`Processing ${filePath}`);
-        let fileContent = await fs.readFile(filePath, 'utf8');
-        for (const regex of regexes) {
-            fileContent = fileContent.replace(regex, match => {
-                // match = match.replace(/\\/g, '');
-                match = decodeURIComponent(match);
-                const remoteResource = remoteResourceData.find(resource => resource.localFilePath === `${PROD_DIRECTORY}${match}`);
-                const relinkedUrl = remoteResource?.isSuccessful ? remoteResource.url : `/${BASE_URL}${match}`;
-                console.log(`Replaced ${match} --> ${relinkedUrl}`);
-                return relinkedUrl;
-            });
-        }
-        await fs.writeFile(filePath, fileContent);
-    }
-}
+// async function relinkUrls(remoteResourceData: RemoteResourceData[]) {
+//     const htmlRegex = /(?<=(href|src)=")\/.*?(?=")/g;
+//     const cssRegex1 = /(?<=url\(')\/.*?(?='\))/g;
+//     const cssRegex2 = /(?<=url\(")\/.*?(?="\))/g;
+//     // 
+//     await Promise.all([
+//         ...htmlFiles.map(filePath => relinkFileUrls(filePath, [htmlRegex])),
+//         ...cssFiles.map(filePath => relinkFileUrls(filePath, [cssRegex1, cssRegex2]))
+//     ]);
+//     return;
+//     // 
+//     async function relinkFileUrls(filePath: string, regexes: RegExp[]) {
+//         console.log(`Processing ${filePath}`);
+//         let fileContent = await fs.readFile(filePath, 'utf8');
+//         for (const regex of regexes) {
+//             fileContent = fileContent.replace(regex, match => {
+//                 // match = match.replace(/\\/g, '');
+//                 match = decodeURIComponent(match);
+//                 const remoteResource = remoteResourceData.find(resource => resource.localFilePath === `${PROD_DIRECTORY}${match}`);
+//                 const relinkedUrl = remoteResource?.isSuccessful ? remoteResource.url : `/${BASE_URL}${match}`;
+//                 console.log(`Replaced ${match} --> ${relinkedUrl}`);
+//                 return relinkedUrl;
+//             });
+//         }
+//         await fs.writeFile(filePath, fileContent);
+//     }
+// }
 
 async function changeBaseUrlTags() {
     const baseUrlTagRegex = /<meta base-url="[^"]*">/g;
@@ -73,6 +73,8 @@ async function changeBaseUrlTags() {
     async function changeBaseUrlTag(filePath: string, regex: RegExp) {
         let fileContent = await fs.readFile(filePath, 'utf8');
         fileContent = fileContent.replace(regex, `<meta base-url="${BASE_URL}">`);
+        console.log(`Changed base-url tag in ${filePath}`);
+        console.log(fileContent);
         await fs.writeFile(filePath, fileContent);
     }
 }
