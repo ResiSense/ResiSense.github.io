4ec247d220a87311c29ccad28c6ebb8c53785eb9
CCheukKa <ccheukka@gmail.com>
2024-08-04 01:57:22 +0800

    fix scrolling and heading jank

--- scripts/globalLibrary.ts
+++ scripts/globalLibrary.ts
@@ -1,3 +1,17 @@
 window.setTimeout(() => {
     document.body.classList.add('post-buffered');
 }, 100);
+
+
+function convertToPixels(element: Element | HTMLElement, value: string): number {
+    const tempElement = document.createElement('div');
+    tempElement.style.position = 'absolute';
+    tempElement.style.visibility = 'hidden';
+    tempElement.style.height = value;
+    element.appendChild(tempElement);
+    const pixels = window.getComputedStyle(tempElement).height;
+    element.removeChild(tempElement);
+    return parseFloat(pixels);
+}
+
+export { convertToPixels };
--- scripts/outline.ts
+++ scripts/outline.ts
@@ -1,8 +1,12 @@
+import { convertToPixels } from "./globalLibrary";
+
 {
     if (window.location.pathname.split('/').includes('404')) {
         const outlineElement = document.querySelector('painted-outline') as HTMLElement;
         outlineElement.style.visibility = 'hidden';
     }
+
+    const HEADING_TOLERANCE_PX = 1;
 
     const outlineProgressPointerElement = document.getElementById('outline-progress-pointer');
     const contentHeadingElements = document.querySelector('painted-content')?.querySelectorAll('h1, h2, h3, h4, h5, h6');
@@ -13,7 +17,12 @@
         if (!outlineProgressPointerElement) { return; }
         const latestHeading =
             [...contentHeadingElements].reverse()
-                .find(heading => heading.getBoundingClientRect().top < 180) || contentHeadingElements[0];
+                .find(heading => {
+                    const headingTop = heading.getBoundingClientRect().top;
+                    const headingTopMin = convertToPixels(heading, getComputedStyle(heading).getPropertyValue('--this-top'));
+                    const isLatestHeading = headingTop <= headingTopMin + HEADING_TOLERANCE_PX;
+                    return isLatestHeading;
+                }) || contentHeadingElements[0];
         const currentProgressOutlineElement = document.getElementById(`--outline-${latestHeading.id}`) as HTMLElement;
         const firstProgressOutlineElement = document.getElementById(`--outline-${contentHeadingElements[0].id}`) as HTMLElement;
         outlineProgressPointerElement.style.top = `${currentProgressOutlineElement.getBoundingClientRect().top - firstProgressOutlineElement.getBoundingClientRect().top}px`;
