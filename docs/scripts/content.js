f5e2c4df6b79c0206318245f85a2758b614abfe7
CCheukKa <ccheukka@gmail.com>
2024-08-04 06:32:46 +0800

    STATIC BUILD: add auto text fragment stripping [skip ci] (#90)
    Co-authored-by: CCheukKa <CCheukKa@users.noreply.github.com>

--- docs/scripts/content.js
+++ docs/scripts/content.js
@@ -1,28 +1,28 @@
 (() => {
-    function e(e, t, o, r) {
-        Object.defineProperty(e, t, { get: o, set: r, enumerable: !0, configurable: !0 });
+    function e(e, t, o, n) {
+        Object.defineProperty(e, t, { get: o, set: n, enumerable: !0, configurable: !0 });
     }
     var t = globalThis,
         o = {},
-        r = {},
-        n = t.parcelRequire8bfa;
-    null == n &&
-        (((n = function (e) {
+        n = {},
+        r = t.parcelRequire8bfa;
+    null == r &&
+        (((r = function (e) {
             if (e in o) return o[e].exports;
-            if (e in r) {
-                var t = r[e];
-                delete r[e];
-                var n = { id: e, exports: {} };
-                return (o[e] = n), t.call(n.exports, n, n.exports), n.exports;
+            if (e in n) {
+                var t = n[e];
+                delete n[e];
+                var r = { id: e, exports: {} };
+                return (o[e] = r), t.call(r.exports, r, r.exports), r.exports;
             }
             var l = Error("Cannot find module '" + e + "'");
             throw ((l.code = 'MODULE_NOT_FOUND'), l);
         }).register = function (e, t) {
-            r[e] = t;
+            n[e] = t;
         }),
-        (t.parcelRequire8bfa = n)),
-        (0, n.register)('df1N6', function (t, o) {
-            function r(e) {
+        (t.parcelRequire8bfa = r)),
+        (0, r.register)('df1N6', function (t, o) {
+            function n(e) {
                 return encodeURIComponent(e)
                     .replace(/[-]/g, '%2D')
                     .replace(/[.]/g, '%2E')
@@ -34,32 +34,36 @@
                     .replace(/[)]/g, '%29')
                     .replace(/[,]/g, '%2C');
             }
-            function n(e, t) {
+            function r(e, t) {
                 let o = document.createElement('div');
                 (o.style.position = 'absolute'),
                     (o.style.visibility = 'hidden'),
                     (o.style.height = t),
                     e.appendChild(o);
-                let r = window.getComputedStyle(o).height;
-                return e.removeChild(o), parseFloat(r);
+                let n = window.getComputedStyle(o).height;
+                return e.removeChild(o), parseFloat(n);
             }
-            e(t.exports, 'safeURIEncode', () => r),
-                e(t.exports, 'convertToPixels', () => n),
+            e(t.exports, 'safeURIEncode', () => n),
+                e(t.exports, 'convertToPixels', () => r),
                 window.setTimeout(() => {
                     document.body.classList.add('post-buffered');
-                }, 100);
+                }, 100),
+                window.addEventListener('DOMContentLoaded', function () {
+                    let e = performance.getEntriesByType('navigation')[0].name;
+                    e.includes('#:~:text=') && window.history.replaceState(null, '', e.split('#')[0]);
+                });
         });
-    var l = n('df1N6');
+    var l = r('df1N6');
     {
         let e = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
             t = document.getElementsByTagName('painted-content')[0],
             o = t.querySelectorAll(e.join(', ')),
-            r = {};
+            n = {};
         function i() {
             for (let e of o) e.classList.toggle('sticky', !1);
             for (let e of o) {
                 let t = e.tagName.toLowerCase(),
-                    o = e.getBoundingClientRect().top <= (r[t] ?? 0);
+                    o = e.getBoundingClientRect().top <= (n[t] ?? 0);
                 if ((e.classList.toggle('sticky', o), !o)) break;
             }
         }
@@ -76,7 +80,7 @@
         }
         e.forEach(e => {
             let o = t.getElementsByTagName(e)[0];
-            o && (r[e] = (0, l.convertToPixels)(o, getComputedStyle(o).getPropertyValue('--this-top')) + 1);
+            o && (n[e] = (0, l.convertToPixels)(o, getComputedStyle(o).getPropertyValue('--this-top')) + 1);
         }),
             document.addEventListener('DOMContentLoaded', i),
             document.addEventListener('scroll', i),
