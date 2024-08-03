f63d519aa11f302b0b5d74d0d9301ecf06af8f01
CCheukKa <ccheukka@gmail.com>
2024-08-04 05:15:03 +0800

    STATIC BUILD: Merge pull request #88 from ResiSense/search-link-goto-text  add links with text highlight [skip ci] (#89)
    Co-authored-by: CCheukKa <CCheukKa@users.noreply.github.com>

--- docs/scripts/outline.js
+++ docs/scripts/outline.js
@@ -1,50 +1,61 @@
 (() => {
-    var e = globalThis,
-        t = {},
+    function e(e, t, n, o) {
+        Object.defineProperty(e, t, { get: n, set: o, enumerable: !0, configurable: !0 });
+    }
+    var t = globalThis,
         n = {},
-        i = e.parcelRequire8bfa;
+        o = {},
+        i = t.parcelRequire8bfa;
     null == i &&
         (((i = function (e) {
-            if (e in t) return t[e].exports;
-            if (e in n) {
-                var i = n[e];
-                delete n[e];
-                var o = { id: e, exports: {} };
-                return (t[e] = o), i.call(o.exports, o, o.exports), o.exports;
+            if (e in n) return n[e].exports;
+            if (e in o) {
+                var t = o[e];
+                delete o[e];
+                var i = { id: e, exports: {} };
+                return (n[e] = i), t.call(i.exports, i, i.exports), i.exports;
             }
             var l = Error("Cannot find module '" + e + "'");
             throw ((l.code = 'MODULE_NOT_FOUND'), l);
         }).register = function (e, t) {
-            n[e] = t;
+            o[e] = t;
         }),
-        (e.parcelRequire8bfa = i)),
-        (0, i.register)('df1N6', function (e, t) {
-            function n(e, t) {
+        (t.parcelRequire8bfa = i)),
+        (0, i.register)('df1N6', function (t, n) {
+            function o(e) {
+                return encodeURIComponent(e)
+                    .replace(/[-]/g, '%2D')
+                    .replace(/[.]/g, '%2E')
+                    .replace(/[_]/g, '%5F')
+                    .replace(/[~]/g, '%7E')
+                    .replace(/[*]/g, '%2A')
+                    .replace(/['']/g, '%27')
+                    .replace(/[(]/g, '%28')
+                    .replace(/[)]/g, '%29')
+                    .replace(/[,]/g, '%2C');
+            }
+            function i(e, t) {
                 let n = document.createElement('div');
                 (n.style.position = 'absolute'),
                     (n.style.visibility = 'hidden'),
                     (n.style.height = t),
                     e.appendChild(n);
-                let i = window.getComputedStyle(n).height;
-                return e.removeChild(n), parseFloat(i);
+                let o = window.getComputedStyle(n).height;
+                return e.removeChild(n), parseFloat(o);
             }
-            Object.defineProperty(e.exports, 'convertToPixels', {
-                get: () => n,
-                set: void 0,
-                enumerable: !0,
-                configurable: !0,
-            }),
+            e(t.exports, 'safeURIEncode', () => o),
+                e(t.exports, 'convertToPixels', () => i),
                 window.setTimeout(() => {
                     document.body.classList.add('post-buffered');
                 }, 100);
         });
-    var o = i('df1N6');
+    var l = i('df1N6');
     {
         window.location.pathname.split('/').includes('404') &&
             (document.querySelector('painted-outline').style.visibility = 'hidden');
         let e = document.getElementById('outline-progress-pointer'),
             t = document.querySelector('painted-content')?.querySelectorAll('h1, h2, h3, h4, h5, h6');
-        function l() {
+        function r() {
             if (!t || !e) return;
             let n =
                     [...t]
@@ -52,14 +63,14 @@
                         .find(
                             e =>
                                 e.getBoundingClientRect().top <=
-                                (0, o.convertToPixels)(e, getComputedStyle(e).getPropertyValue('--this-top')) + 1
+                                (0, l.convertToPixels)(e, getComputedStyle(e).getPropertyValue('--this-top')) + 1
                         ) || t[0],
-                i = document.getElementById(`--outline-${n.id}`),
-                l = document.getElementById(`--outline-${t[0].id}`);
-            (e.style.top = `${i.getBoundingClientRect().top - l.getBoundingClientRect().top}px`),
-                (e.style.left = `${2 + i.getBoundingClientRect().left - l.getBoundingClientRect().left}px`);
+                o = document.getElementById(`--outline-${n.id}`),
+                i = document.getElementById(`--outline-${t[0].id}`);
+            (e.style.top = `${o.getBoundingClientRect().top - i.getBoundingClientRect().top}px`),
+                (e.style.left = `${2 + o.getBoundingClientRect().left - i.getBoundingClientRect().left}px`);
         }
-        window.addEventListener('DOMContentLoaded', l), window.addEventListener('scroll', l);
+        window.addEventListener('DOMContentLoaded', r), window.addEventListener('scroll', r);
     }
 })();
 //# sourceMappingURL=outline.js.map
